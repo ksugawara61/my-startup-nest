bbba825c8d9b36129fee419f20c13ea55f7f722a
ksugawara61 <ksugawara61@users.noreply.github.com>
2022-02-13 14:19:56 +0900

    Add: もろもろ追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
+import { Cats } from './cats';
+import { CatsService } from './cats/cats.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CatsController],
+  providers: [AppService, Cats, CatsService],
 })
 export class AppModule {}
