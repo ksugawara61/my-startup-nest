fdde8087352d66c411c76409f863f405630be5f7
ksugawara61 <ksugawara61@users.noreply.github.com>
2022-02-13 22:25:41 +0900

    Add: users repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,24 @@ import {
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/logger.middleware';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CatsModule, TypeOrmModule.forRoot()],
+  imports: [
+    CatsModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'mysql',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
