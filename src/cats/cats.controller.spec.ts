79a21395cf9f88f8a823ae5106dd9198b990bf54
ksugawara61 <ksugawara61@users.noreply.github.com>
2022-02-13 16:11:19 +0900

    Fix: test

--- src/cats/cats.controller.spec.ts
+++ src/cats/cats.controller.spec.ts
@@ -1,18 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 describe('CatsController', () => {
-  let controller: CatsController;
+  let catsController: CatsController;
+  let catsService: CatsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CatsController],
+      providers: [CatsService],
     }).compile();
 
-    controller = module.get<CatsController>(CatsController);
+    catsService = module.get<CatsService>(CatsService);
+    catsController = module.get<CatsController>(CatsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of cats', async () => {
+      const cat: Cat = { id: 1, name: 'mike', age: 3, breed: 'k2o' };
+      const result = [cat];
+      jest.spyOn(catsService, 'findAll').mockImplementation(() => result);
+      expect(await catsController.findAll()).toBe(result);
+    });
   });
 });
